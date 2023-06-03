dae90acd94670aced2bb29adafc56928fcdf9723
definitelynotamit <92351713+definitelynotamit@users.noreply.github.com>
2023-06-04 01:42:15 +0530

    Update settings.py

--- classifiedads/settings.py
+++ classifiedads/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -175,7 +175,7 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # MAILER_EMAIL_BACKEND = EMAIL_BACKEND
